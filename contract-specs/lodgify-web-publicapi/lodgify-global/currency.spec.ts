d42ec83f3a8ebf7c56e0ad5ab6b4797e78835dc4
Nelson Salazar <nelsonds_94@hotmail.com>
2024-05-27 12:19:09 +0200

    QA-2934: contracts for currency and image

---
+++ contract-specs/lodgify-web-publicapi/lodgify-global/currency.spec.ts
@@ -0,0 +1,44 @@
+/**
+ * @jest-environment node
+ */
+
+import { PactV4 } from "@pact-foundation/pact";
+import path from "path";
+
+const pact = new PactV4({
+  dir: path.resolve(
+    process.cwd(),
+    "contract-specs/lodgify-web-publicapi/pacts"
+  ),
+  consumer: "lodgify-web-publicapi",
+  provider: "lodgify-global",
+});
+
+describe("Pact Lodgify Consumer", () => {
+  describe("/api/v2/currency", () => {
+    test("GET - /api/v2/currency", async () => {
+      await pact
+        .addInteraction()
+        .uponReceiving("A request for list of currencies")
+        .withRequest("GET", "/api/v2/currency")
+        .willRespondWith(200, (builder) => {
+          builder.jsonBody([
+            {
+              id: 50,
+              code: "USD",
+              name: "US dollar",
+              euroForex: 1.0865,
+              symbol: "$  ",
+              obsolete: false,
+              decimalPlaces: 2,
+            },
+          ]);
+        })
+        .executeTest((mockServer) => {
+          return fetch(`${mockServer.url}/api/v2/currency`).then((res) =>
+            expect(res.status).toBe(200)
+          );
+        });
+    });
+  });
+});
---
+++ contract-specs/lodgify-web-publicapi/lodgify-global/image.spec.ts
@@ -0,0 +1,45 @@
+/**
+ * @jest-environment node
+ */
+
+import { PactV4 } from "@pact-foundation/pact";
+import path from "path";
+
+const pact = new PactV4({
+  dir: path.resolve(
+    process.cwd(),
+    "contract-specs/lodgify-web-publicapi/pacts"
+  ),
+  consumer: "lodgify-web-publicapi",
+  provider: "lodgify-global",
+});
+
+describe("Pact Lodgify Consumer", () => {
+  describe("/api/v1/image/{id}", () => {
+    test("GET - /api/v1/image/{id}", async () => {
+      await pact
+        .addInteraction()
+        .uponReceiving("A request for an image file")
+        .withRequest(
+          "GET",
+          "/api/v1/image/b59337ac-4017-45a3-903b-69206f68fee2"
+        )
+        .willRespondWith(200, (builder) => {
+          builder.jsonBody({
+            id: "b59337ac-4017-45a3-903b-69206f68fee2",
+            width: 100,
+            height: 100,
+            size: 2420448,
+            dateCreated: "2023-01-03T22:44:05",
+            dateModified: "2023-01-03T22:44:05",
+            type: "Jpeg",
+          });
+        })
+        .executeTest((mockServer) => {
+          return fetch(
+            `${mockServer.url}/api/v1/image/b59337ac-4017-45a3-903b-69206f68fee2`
+          ).then((res) => expect(res.status).toBe(200));
+        });
+    });
+  });
+});
